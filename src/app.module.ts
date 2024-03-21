4afc1cb22160d901b9825a5e10461a4b174a6aa0
Anton D <adkv.lab@gmail.com>
2024-03-21 13:40:13 +0100

    refactor: change mode env

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ config();
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      synchronize: process.env.MODE === 'development',
+      synchronize: process.env.NODE_ENV === 'development',
       autoLoadEntities: true,
     }),
     UserModule,
